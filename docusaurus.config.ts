fbc05bad21a345902117e95cb11d532e162c0603
tbsbdr <t.baader@opencloud.eu>
2025-02-17 22:57:44 +0100

    accessible link color (light mode)
    accessible link color with enough contrast according to AA contrast requirement.
    enables light mode again.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,8 +54,8 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      defaultMode: 'dark',
-      disableSwitch: true,
+      defaultMode: 'light',
+      disableSwitch: false,
       respectPrefersColorScheme: false,
     },
     navbar: {
